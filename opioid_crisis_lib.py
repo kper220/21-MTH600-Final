13a96a05e2c392312ed6891a09400d219c370088
David <36579697+kper220@users.noreply.github.com>
2021-11-11 22:23:21 -0500

    updates

--- opioid_crisis_lib.py
+++ opioid_crisis_lib.py
@@ -1,6 +1,36 @@
 # opioid crisis analysis library.
+# David Li, MTH 600.
 
 import pandas as pd
+
+# one-to-one correspondence between states and initials.
+state_in = {
+        "kentucky": "ky",
+        "ohio": "oh",
+        "pennsylvania": "pa",
+        "virginia": "va",
+        "west virginia": "wv",
+        }
+in_state = {v: k for k, v in state_in.items()}
+
+# given state and county, retrieve longitude and latitude.
+
+def locate(state_in, county, df_geo):
+    """
+    return lattitude and longitude coordinates given state initials and county.
+    """
+    county = county.lower() + " county"
+    indexes = [i for i, cty in enumerate(df_geo["NAME"]) if cty.lower() == county]
+
+    for i in indexes:
+        # check if state agrees with input state.
+        if list(df_geo["USPS"])[i].lower() == state_in.lower():
+            lat = list(df_geo["INTPTLAT"])[i]
+            lon = list(df_geo["INTPTLONG                                                                                                               "])[i]
+            # I don't know why INTPLTLONG is like this.
+
+            return (lat, lon)
+
 
 # dataframe analysis.
 def feature_extract(df, df_metadata):
@@ -26,6 +56,11 @@ def feature_extract(df, df_metadata):
             continue
         if int(key[3])%2 == 0:
             continue
+
+        desc = df[key].iloc[0] # get description
+        desc = desc.split() # split to words.
+        if desc[0] == "Percent;":
+            continue # reject percentage estimates.
         
         # also skip if column of feature is string.
         first_dat = df[key].iloc[1] # get first piece of data.
@@ -45,6 +80,11 @@ def feature_index(ddf, ddf_metadata):
     with corresponding labels in their respective years.
     """
 
+    def _get_key(df, desc):
+        # get the key from a df corresponding to desc.
+        i = list(df.iloc[0]).index(desc)
+        return df.keys()[i]
+
     n = len(ddf) # number of dataframes
     total_features = [None for _ in range(n)]
 
@@ -61,9 +101,30 @@ def feature_index(ddf, ddf_metadata):
 
     # get all map keys.
     total_keys = [f.keys() for f in total_features]
-    univ_desc = list(set(total_keys[0]).intersection(*total_keys[1:]))
+    univ_desc = ["Geography"] + list(set(total_keys[0]).intersection(*total_keys[1:]))
 
-    return univ_desc
+    # define a universal map.
+    univ_map = dict()
+
+    for desc in univ_desc:
+        univ_map[desc] = [None for _ in range(n)]
+        for i in range(n):
+            _df = ddf[i]
+            _df_metadata = ddf_metadata[i]
+
+            univ_map[desc][i] = _get_key(_df, desc)
+
+    return univ_map
+
+def state_and_county(geography):
+    """
+    Takes geography data from socio-economic factors,
+    returns initials of state and county name (without " county")
+    """
+    county, state = geography.split(", ") # separate county from state
+    state = state_in[state.lower()] # convert state to initials
+    county = ' '.join(county.split()[:-1]) # remove the word "county"
+    return (state, county)
 
 if __name__ == "__main__":
 
