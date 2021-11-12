703f41e94786172ab01a778ba56564a12190fb0e
David <36579697+kper220@users.noreply.github.com>
2021-11-12 14:38:48 -0500

    update

--- opioid_crisis_lib.py
+++ opioid_crisis_lib.py
@@ -6,14 +6,14 @@ from numpy import array
 import pandas as pd
 
 # one-to-one correspondence between states and initials.
-state_in = {
+state_in_dict = {
         "kentucky": "ky",
         "ohio": "oh",
         "pennsylvania": "pa",
         "virginia": "va",
         "west virginia": "wv",
         }
-in_state = {v: k for k, v in state_in.items()}
+in_state_dict = {v: k for k, v in state_in_dict.items()}
 
 # given state and county, retrieve longitude and latitude.
 
@@ -74,7 +74,52 @@ def feature_extract(df, df_metadata):
 
     return features
 
-def feature_index(ddf, ddf_metadata):
+def feature_index2(ddf_yyyy, ddf_metadata_yyyy, include_geography=False):
+    """
+    Takes a list of dataframes and metadataframes (indexed by year).
+    Returns dictionary of universally accessible features
+    with corresponding labels in their respective years.
+    """
+
+    def _get_key(df, desc):
+        # get key from df corresponding to desc.
+        i = list(df.iloc[0]).index(desc)
+        return df.keys()[i]
+
+    n = len(ddf_yyyy) # number of dataframes.
+    total_features = [None for _ in range(n)]
+    
+    for i, year in enumerate(ddf_yyyy.keys()):
+        _f_extract = feature_extract(ddf_yyyy[year], ddf_metadata_yyyy[year])[1:] # get features.
+        _f_dict = dict() # create dictionary map: set of features to set of labels (which are indexed by year).
+
+        for f in _f_extract:
+            desc = ddf_yyyy[year][f].iloc[0] # get description.
+            _f_dict[desc] = f
+        
+        total_features[i] = _f_dict # map from description to label
+
+    # get all map keys.
+    total_keys = [f.keys() for f in total_features]
+    if include_geography:
+        univ_desc = ["Geography"] + list(set(total_keys[0]).intersection(*total_keys[1:]))
+    else:
+        univ_desc = list(set(total_keys[0]).intersection(*total_keys[1:]))
+
+    # define the universal map: features -> (year -> label)
+    univ_map = dict()
+    for desc in univ_desc:
+        univ_map[desc] = dict() # map: year -> label.
+        
+        for year in ddf_yyyy.keys():
+            _df = ddf_yyyy[year]
+            _df_metadata = ddf_metadata_yyyy[year]
+            univ_map[desc][year] = _get_key(_df, desc)
+
+    return univ_map
+
+
+def feature_index(ddf, ddf_metadata, include_geography=False):
     """
     Takes a list of dataframes and metadataframes.
     Returns dictionary of universally accessible features
@@ -102,7 +147,11 @@ def feature_index(ddf, ddf_metadata):
 
     # get all map keys.
     total_keys = [f.keys() for f in total_features]
-    univ_desc = ["Geography"] + list(set(total_keys[0]).intersection(*total_keys[1:]))
+
+    if include_geography:
+        univ_desc = ["Geography"] + list(set(total_keys[0]).intersection(*total_keys[1:]))
+    else:
+        univ_desc = list(set(total_keys[0]).intersection(*total_keys[1:]))
 
     # define a universal map.
     univ_map = dict()
@@ -120,12 +169,13 @@ def feature_index(ddf, ddf_metadata):
 def state_and_county(geography):
     """
     Takes geography data from socio-economic factors,
-    returns initials of state and county name (without " county")
+    returns state and county name (without " county")
     """
     county, state = geography.split(", ") # separate county from state
-    state = state_in[state.lower()] # convert state to initials
+    state_in = state_in_dict[state.lower()] # convert state to initials
     county = ' '.join(county.split()[:-1]) # remove the word "county"
-    return (state, county)
+
+    return state_in, county, state
 
 def drug_matrix(df_nflis, substanceNamesDict):
     """
@@ -180,6 +230,56 @@ def drug_vector(yyyy, state_in, county, df_nflis, substanceNamesDict, identify=F
         return d_vec, drug_dict
     else:
         return d_vec
+
+def generate_sample(ddf_yyyy, ddf_yyyy_meta, f_index, df_nflis, substanceNamesDict, df_geo):
+    """
+    Takes relevant socio-economic dataframes, metadataframes,
+    applicable (universal) socio-economic features and drug use data.
+    Returns an appropriate sample matrix with explanatory variables followed by response variables.
+    """
+
+    # determine dimension.
+    m = 0 # sample size.
+    
+    df_sample_sizes = [ddf_yyyy[yyyy].shape[0] - 1 for yyyy in ddf_yyyy] # -1 so we exclude row with labels.
+    m = sum(df_sample_sizes)
+
+    geo_n = 2 # positional features: longitude and latitude.
+    socio_n = len(f_index) # non-positional, socio-economic features.
+    drug_n = len(substanceNamesDict) # drug type features.
+    n = geo_n + socio_n + drug_n # feature dimension equals sum of above three features.
+
+    sample = np.zeros((m, n)) # instantiate mxn sample matrix.
+    
+    # fill sample.
+    for l, year in enumerate(ddf_yyyy):
+        df = ddf_yyyy[year] # dataframe for this year.
+        df_msum = sum(df_sample_sizes[:l]) # samples processed by previous dataframes.
+        df_m = df_sample_sizes[l] # current dataframe's sample size.
+
+        for i in range(df_m):
+            i_sample = i + df_msum # index in the sample matrix
+            i_df = i + 1 # index in the dataframe
+            # append geographic data.
+            try:
+                state_in, county = state_and_county(df["GEO.display-label"].iloc[i_df])[:2] # retrieve state initials, county.
+            except:
+                print(l)
+                print(i)
+                print(i_df)
+                print(df["GEO.display-label"].iloc[i_df])
+                raise
+            try:
+                lat, lon = locate(state_in, county, df_geo) # get lattitude, longitude
+            except TypeError:
+                lat = None
+                lon = None
+            sample[i_sample][0] = lat
+            sample[i_sample][1] = lon
+
+            # append socio-economic data.
+
+    return sample
 
 if __name__ == "__main__":
 
